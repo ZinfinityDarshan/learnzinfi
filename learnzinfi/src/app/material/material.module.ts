63e99524966d8598f2e153f872d481deecd037d2
ZinfinityDarshan <darshan.redkar@zinfinity.in>
2019-10-04 12:07:06 +0000

    third commit

--- learnzinfi/src/app/material/material.module.ts
+++ learnzinfi/src/app/material/material.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTooltipModule } from "@angular/material/tooltip";
 
 @NgModule({
   declarations: [],
@@ -10,12 +12,16 @@ import { MatToolbarModule } from "@angular/material/toolbar";
     CommonModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatTooltipModule
   ],
   exports:[
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatTooltipModule
   ]
 })
 export class MaterialModule { }
